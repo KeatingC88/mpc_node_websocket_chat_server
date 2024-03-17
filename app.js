9f3bb8d80ea9b058f8d9e35742a4a8c9e6dddde5
keatingc88@gmail.com <keatingc88@gmail.com>
2024-03-17 06:53:02 -0600

    installed redis and user log

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 `use strict`
 const express = require(`express`)
 const expressWs = require('express-ws')
+const redis = require('redis')
 const cluster = require(`node:cluster`)
 const totalCPUs = require("os").cpus().length
 const port = 8082
-const serverLog = {}
+
+const ServerSessionLog = {}
+
 const axios = require("axios")
 
 if (cluster.isPrimary) {
+    //Predatabase operations...
+    (async () => {
+        const redisClient = redis.createClient({
+            socket: {
+                host: "127.0.0.1",
+                port: 6379,
+                username: "default",
+                password: ""
+            }
+        })
+        redisClient.on("error", (error) => console.error(`Error : ${error}`))
+        await redisClient.connect()
+        console.log(`Redis is Connected to NodeJS.`)
+    })().catch(err => {
+        console.error(err);
+    })
+
     for (let i = 0; i < totalCPUs; i++) {
         cluster.fork()
     }
+
     cluster.on('exit', (worker, code, signal) => {
         cluster.fork()
     })
@@ -22,18 +43,27 @@ if (cluster.isPrimary) {
     const wsHandler = (ws) => {
         connections.add(ws)
 
-        ws.on('message', (message) => {
-            console.log(message)//Save per message?
-            serverLog[Object.keys(serverLog).length] = message
-            console.log(serverLog)//Save per X amount messages?
-            connections.forEach((conn) => conn.send(message))
+        ws.on('message', async (message_string) => {
+            let obj = await JSON.parse(message_string)
+            const redisClient = await redis.createClient({
+                socket: {
+                    host: "127.0.0.1",
+                    port: 6379,
+                    username: "default",
+                    password: ""
+                }
+            })
+            await redisClient.on("error", (error) => console.error(`Error : ${error}`))
+            await redisClient.connect()
+            await redisClient.rPush(`${obj.id} ${obj.send_to} ${obj.timestamp}`, [`${obj.display_name}`, `${obj.id}`, `${obj.send_to}`, `${obj.message}`, `${obj.online_status}`, `${obj.avatar_url_path}`, `${obj.avatar_title}`, `${obj.timestamp}`])
+            await connections.forEach((conn) => conn.send(message_string))
         })
 
         ws.on('close', () => {
             connections.delete(ws)
         })
     }
-    app.ws('/api/chat', wsHandler)
+    app.ws('/api/chat_universe', wsHandler)
     app.use(express.static('build'))
     app.listen(port, () => console.log(`WSChat Server: a thread is listening on PORT ${port}`))
 }
